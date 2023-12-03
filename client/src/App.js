89cb3eb0f4a9fc1d57db0d9a4423a80a27d38dda
samuel-carr <samuelcarr.official@gmail.com>
2023-12-03 21:54:48 +0530

    imported env and uri from environment

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 
 const dotenv_config = dotenv.config();
 
+const env = process.env.REACT_ENV || 'development';
+const api_base_uri = process.env.REACT_API_URI || 'http://localhost:5000/api';
+
 function App() {
   return (
     <div className="App">
