26826a1fc3891cf363c6cd9e73e8359f2f1f0250
samuel-carr <samuelcarr.official@gmail.com>
2023-12-03 22:16:52 +0530

    added simple check endpoint

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const env = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 5000;
-const uri = process.env.ATLAS_URI;
+const uri = process.env.NODE_ATLAS_URI;
 
 // middleware
 app.use(cors());
@@ -47,4 +47,9 @@ connectMongoDBAsync();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/api/check', (req, res) => {
+
+	res.json({ message: 'Hello from the API!' });
 });
