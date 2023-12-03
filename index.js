0f8bdd70aef62762813210947cc18e6eb40801af
samuel-carr <samuelcarr.official@gmail.com>
2023-12-03 16:17:29 +0530

    Initial connection to mongodb atlas

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(uri);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
