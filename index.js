5fdb2619277aa86d9a67ea69ef27d8d2a2211e0f
samuel-carr <samuelcarr.official@gmail.com>
2023-12-03 16:12:21 +0530

    created simple express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
